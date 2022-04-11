2999b9cba92f5e60615ab05cbaea507b3393c0c6
Valentin <zvezdochet.79@mail.ru>
2022-04-11 13:30:31 +0300

    Задачи 1 и 2

---
+++ Задача1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+//все чётные натуральные числа в промежутке от M до N.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int x = Prompt("Введите число 1: ");
+int y = Prompt("Введите число 2: ");
+while (y == x)
+{
+    y = Prompt("Не-не-не, другое число: ");  // Немножко улыбки
+}
+
+if(x > y) (x, y) = (y, x);  // что бы было не важно, какое число больше. В теле метода вызывает лишний вывод.
+NatureEvenRow(x , y);
+void NatureEvenRow(int x, int y)
+{
+    if(x >= y) return;
+    if(x%2 != 0)  x++;
+    Console.Write(x +", ");
+    NatureEvenRow(x+1 , y);
+}
---
+++ Задача2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int M = Prompt("Введите число M: ");
+int N = Prompt("Введите число N: ");
+if(M > N) (M, N) = (N, M); // меняем большее и меньшее
+
+int SumNumbers(int M, int N)
+{
+    if(M > N) return 0;
+    return SumNumbers(M+1, N) + M;
+}
+
+Console.WriteLine($"Сумма всех элементов = {SumNumbers(M, N)}");
